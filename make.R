2be1d77c8d0bf95e72bed6c38f6bca75596537f5
Joel Nitta <joelnitta@gmail.com>
2019-07-04 09:40:13 +0900

    Specify cache when running plan in parallel

--- make.R
+++ make.R
@@ -1,6 +1,6 @@
 # make.R
 
-# Master script for running Pleurorosiopsis analyses.
+# Master script for running analyses.
 # This project uses drake to manage workflows.
 # For more information about drake, see
 # https://ropensci.github.io/drake/
@@ -19,6 +19,7 @@ source("code/packages.R")
 # Update drake settings
 pkgconfig::set_config("drake::strings_in_dots" = "literals")
 
+
 # Load functions and plans  ----
 source("code/functions.R")
 source("code/plan.R")
@@ -29,21 +30,20 @@ ja_fern_cache <- new_cache("ja_fern_cache")
 # Run analyses ----
 
 # If not running in parallel, just call make(plan)
-make(plan, cache = ja_fern_cache) 
+# make(plan, cache = ja_fern_cache) 
 
 # OR, uncomment out the below lines to run in parallel.
-# 
-# future::plan(future::multiprocess)
-# 
-# tictoc::tic()
-# make(
-#   plan,
-#   parallelism = "future",
-#   jobs = 4,
-#   prework = list(
-#     quote(conflict_prefer("map", "purrr")),
-#     quote(conflict_prefer("select", "dplyr")),
-#     quote(conflict_prefer("filter", "dplyr")),
-#     quote(conflict_prefer("gather", "tidyr"))
-#   ))
-# tictoc::toc()
+
+future::plan(future::multiprocess)
+
+make(
+  plan,
+  parallelism = "future",
+  jobs = 4,
+  cache = ja_fern_cache,
+  prework = list(
+    quote(conflict_prefer("map", "purrr")),
+    quote(conflict_prefer("select", "dplyr")),
+    quote(conflict_prefer("filter", "dplyr")),
+    quote(conflict_prefer("gather", "tidyr"))
+  ))
