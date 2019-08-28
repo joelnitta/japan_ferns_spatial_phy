1aea7238a35bdb37e0b15e0eb32251c71fca6f9f
Joel Nitta <joelnitta@gmail.com>
2019-08-28 14:48:53 -0400

    Add ecos_plot_pie2 function
    To use sf object instead of reading in shape file

--- code/functions.R
+++ code/functions.R
@@ -485,6 +485,83 @@ make_ecos_matrix <- function (comm_pteridos, all_cells) {
   
 }
 
+# Slightly tweaked version of ecos_plot_pie that takes sf object as background map
+# instead of reading in shape file.
+ecos_plot_pie2 <- function (
+  omega = NULL, coords = NULL, bgmap_path = NULL, adjust = FALSE, 
+  thresh = 0.7, long_lim = c(-180, 180), lat_lim = c(-60, 90), 
+  coastline_lwd = 10, intensity = 1, radius = 0.5, color = c("dodgerblue2", 
+                                                             "#E31A1C", "green4", "#6A3D9A", "#FF7F00", "black", "gold1", 
+                                                             "skyblue2", "#FB9A99", "palegreen2", "#CAB2D6", "#FDBF6F", 
+                                                             "gray70", "khaki2", "maroon", "orchid1", "deeppink1", 
+                                                             "blue1", "steelblue4", "darkturquoise", "green1", "yellow4", 
+                                                             "yellow3", "darkorange4", "brown", "red", "cornflowerblue", 
+                                                             "cyan", "brown4", "burlywood", "darkgoldenrod1", "azure4", 
+                                                             "green", "deepskyblue", "yellow", "azure1"), pie_control = list(), 
+  image_width = 1000, image_height = 800, path = "geostructure_plot.tiff") 
+{
+  if (is.null(coords)) {
+    if (is.null(rownames(omega))) {
+      stop("coords not provided, omega rownames do not have latitude longitude\n           information either")
+    }
+    latlong_chars <- rownames(omega)
+    coords <- cbind.data.frame(as.numeric(sapply(latlong_chars, 
+                                                 function(x) strsplit(x, "_")[[1]][1])), as.numeric(sapply(latlong_chars, 
+                                                                                                           function(x) strsplit(x, "_")[[1]][2])))
+    colnames(coords) <- c("lat", "long")
+  }
+  else {
+    if (dim(coords)[1] != dim(omega)[1]) {
+      stop("coords provided, but the number of rows in coords data does not\n           match the number of rows in omega matrix")
+    }
+  }
+  pie_control_default <- list(edges = 200, clockwise = TRUE, 
+                              init.angle = 90, density = NULL, angle = 45, border = NULL, 
+                              lty = NULL, label.dist = 1.1)
+  pie_control <- modifyList(pie_control_default, pie_control)
+  if (is.null(bgmap_path)) {
+    message("reading background map shapefile from inst/extdata/ne_110m_coastline \n            folder")
+    GlobalCoast <- sf::st_read(system.file("extdata", "ne_110m_land", 
+                                           "ne_110m_land.shp", package = "ecostructure"), quiet = T)
+  }
+  else {
+    GlobalCoast <- bgmap_path
+  }
+  glob <- c(xmin = long_lim[1], xmax = long_lim[2], ymin = lat_lim[1], 
+            ymax = lat_lim[2])
+  glob <- sf::st_bbox(glob)
+  glob <- structure(glob, crs = sf::st_crs(GlobalCoast))
+  GlobalCoast <- suppressWarnings(suppressMessages(sf::st_intersection(GlobalCoast, 
+                                                                       sf::st_as_sfc(glob))))
+  if (adjust) {
+    idx <- which(omega[, 1] > thresh)
+    omega <- omega[-idx, ]
+    coords <- coords[-idx, ]
+    omega <- omega[, -1]
+    omega <- t(apply(omega, 1, function(x) return(x/sum(x))))
+  }
+  output_type <- strsplit(path, "[.]")[[1]][2]
+  if (output_type == "tiff") {
+    tiff(path, width = image_width, height = image_height)
+  }
+  else if (output_type == "png") {
+    png(path, width = image_width, height = image_height)
+  }
+  else if (output_type == "pdf") {
+    pdf(path, width = image_width, height = image_height)
+  }
+  else {
+    stop("the output image may either be of  tiff, png or pdf extension")
+  }
+  plot(sf::st_geometry(GlobalCoast), axes = T, main = "", reset = F, 
+       xaxs = "i", yaxs = "i", lwd = coastline_lwd)
+  par(lwd = 0.01)
+  invisible(lapply(1:dim(omega)[1], function(r) do.call(mapplots::add.pie, 
+                                                        append(list(z = as.integer(100 * omega[r, ]), x = coords[r, 
+                                                                                                                 1], y = coords[r, 2], labels = c("", "", ""), radius = radius, 
+                                                                    col = sapply(color, scales::alpha, intensity)), pie_control))))
+  invisible(dev.off())
+}
 
 # Plotting ----
 
