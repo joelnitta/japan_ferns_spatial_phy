f6e8cba150feff35b81ca37c00f0ce38b9c9dd9c
Joel Nitta <joelnitta@gmail.com>
2019-08-31 20:35:52 -0400

    Fix name of R Core Team

--- code/process_refs.R
+++ code/process_refs.R
@@ -7,3 +7,12 @@ make_ref_list(
   rmd_file = "ms/manuscript.Rmd", 
   raw_bib = "ms/references_raw.bib",
   final_bib = "ms/references.bib")
+
+# Make some manual fixes to authors in SI bibliography
+# (these are institutions, so need double brackets to
+# avoid latex thinking they have first and last names)
+read_lines("ms/references.bib") %>%
+  str_replace(
+    "R Core Team",
+    "\\{R Core Team\\}") %>%
+  write_lines("ms/references.bib")
