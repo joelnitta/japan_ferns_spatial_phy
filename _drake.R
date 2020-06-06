bef0fa11626537a1f051035b41ccc1b1e0de3d86
Joel Nitta <joelnitta@gmail.com>
2020-06-06 20:18:45 +0000

    Use base for resolving conflicts
    conflicted is too verbose

--- _drake.R
+++ _drake.R
@@ -20,11 +20,12 @@ drake_config(
   jobs = 6, # Change to match number of cores available!
   cache = ja_fern_cache,
   seed = 0,
+  # Resolve conflicts within each job
   prework = list(
-    quote(conflict_prefer("map", "purrr")),
-    quote(conflict_prefer("select", "dplyr")),
-    quote(conflict_prefer("filter", "dplyr")),
-    quote(conflict_prefer("gather", "tidyr"))
+    quote(map <- purrr::map),
+    quote(select <- dplyr::select),
+    quote(filter <- dplyr::filter),
+    quote(gather <- tidyr::gather)
   )
 )
 
