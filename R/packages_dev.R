a242492ac5582cfeefe4318a2702dfdedff56b0f
Joel Nitta <joelnitta@gmail.com>
2020-07-11 09:29:21 +0000

    Add xarningan, rnaturalearthdata, take snapshot

--- R/packages_dev.R
+++ R/packages_dev.R
@@ -3,6 +3,11 @@
 # but included here so renv knows to keep them around
 library(remotes)
 library(visNetwork)
+library(rnaturalearthdata)
+# don't track rnaturalearthhires with renv, it gives this error upon attempting snapshot:
+# "The following package(s) were installed from an unknown source...
+# renv may be unable to restore these packages in the future."
+# library(rnaturalearthhires)
 
 # considering these...
 library(ggtext)
