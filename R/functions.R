e99963b05baa158970c982c4c05a748b9b011b6e
Joel Nitta <joelnitta@gmail.com>
2020-07-24 21:39:33 +0000

    Return NA instead of erroring for NAs in count_higher, count_lower

--- R/functions.R
+++ R/functions.R
@@ -1327,18 +1327,25 @@ calc_biodiv_random <- function (
 #'
 #' @param x Number to count
 #' @param y Vector of numbers to compare
+#' @param na.rm = Logical; should NA values in the comparison
+#' vector be removed before making comparison?
 #'
 #' @return Number of times x is higher than y
 #' 
 #' @examples {
 #' count_higher(4, 1:10)
+#' count_higher(4, c(1:10, NaN))
 #' }
-count_higher <- function (x, y) {
+count_higher <- function (x, y, na.rm = TRUE) {
   
   assertthat::assert_that(assertthat::is.number(x))
   assertthat::assert_that(is.numeric(y))
-  assertthat::assert_that(!is.na(x))
-  assertthat::assert_that(!anyNA(y))
+  
+  # remove any NAs before making comparison
+  if(isTRUE(na.rm)) y <- y[!is.na(y)]
+  
+  # if comparison is zero length, return NA
+  if(length(y) == 0) return (NaN)
   
   sum((x > y))
 }
@@ -1347,18 +1354,25 @@ count_higher <- function (x, y) {
 #'
 #' @param x Number to count
 #' @param y Vector of numbers to compare
+#' @param na.rm = Logical; should NA values in the comparison
+#' vector be removed before making comparison?
 #'
 #' @return Number of times x is lower than y
 #' 
 #' @examples {
 #' count_lower(4, 1:10)
+#' count_lower(NaN, 1:10)
 #' }
-count_lower <- function (x, y) {
+count_lower <- function (x, y, na.rm = TRUE) {
   
   assertthat::assert_that(assertthat::is.number(x))
   assertthat::assert_that(is.numeric(y))
-  assertthat::assert_that(!is.na(x))
-  assertthat::assert_that(!anyNA(y))
+  
+  # remove any NAs before making comparison
+  if(isTRUE(na.rm)) y <- y[!is.na(y)]
+  
+  # if comparison is zero length, return NA
+  if(length(y) == 0) return (NaN)
   
   sum((x < y))
 }
