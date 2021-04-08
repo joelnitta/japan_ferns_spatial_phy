125dbb9f0c5f695a6995a8ce1a53a99563e7565d
Joel Nitta <joelnitta@gmail.com>
2021-04-08 15:56:49 +0900

    Add script for running targets with RStudio build feature

---
+++ run.R
@@ -0,0 +1,4 @@
+#!/usr/bin/env Rscript
+# This enables running targets using RStudio build tools
+# https://ulhpc-tutorials.readthedocs.io/en/latest/maths/R/PS10_targets.html#19
+targets::tar_make()
